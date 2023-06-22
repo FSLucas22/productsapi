fc38e2eb68de0399e2a31580d14cbacce5cab762
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-06-22 01:10:24 -0300

    Implemented roles

--- src/main/java/com/api/products/config/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/products/config/security/UserDetailsServiceImpl.java
@@ -1,8 +1,9 @@
 package com.api.products.config.security;
 
 import com.api.products.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,13 +11,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByUsername(username).orElseThrow(
+        var userModel =  repository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("User not found for username: " + username));
+
+        return new User(userModel.getUsername(), userModel.getPassword(), userModel.getAuthorities());
     }
 }
---
+++ src/main/java/com/api/products/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.api.products.enums;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER;
+}
